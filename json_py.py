bddfd8ca8bfe994ba5b1fef704733ec2914af456
Sanix_sandel <36574404+sanix-sandel@users.noreply.github.com>
2020-11-21 05:58:31 +0300

    Update json_py.py

--- json_py.py
+++ json_py.py
@@ -3,12 +3,17 @@ import json
 from jsonschema import validate
 
 if __name__=='__main__':
+        subfolders = [ f.name for f in os.scandir(os.getcwd()) if f.is_dir() ]
         
+        folder1, folder2=[f for f in subfolders if f=='event'][0],[f for f in subfolders if f=='schema'][0]
 
-        path1=os.listdir('event')          
+        #path1=os.listdir('event')          
        # print(path1)
-        path2=os.listdir('schema')
+        #path2=os.listdir('schema')
        # print(path2)  
+        path1=os.listdir(folder1)          
+       
+        path2=os.listdir(folder2)
         errors=0
         for filename in path1:
                 with open('event/{}'.format(filename)) as file:
@@ -21,6 +26,11 @@ if __name__=='__main__':
                                         except Exception as e:
                                                 print("error occured")
                                                 #print(e) //To see all the errors occured remove #
+                                                with open('errors.txt', 'a') as error_file:
+                                                        print("error occured in file "+filename)
+                                                
+                                                        error_file.write("Error occured in file "+filename)
+                                                        error_file.write(e.message+"\n")
                                                 errors+=1        
                                                                       
 
